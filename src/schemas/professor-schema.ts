0a6074585bf4d88e25bc38fc4e9093e84a4c612f
brunomffl <brunocardoso07@outlook.com>
2025-09-02 17:57:15 -0300

    fix: Removendo tipo Professor do professor-schemas.ts

--- src/schemas/professor-schema.ts
+++ src/schemas/professor-schema.ts
@@ -1,17 +1,5 @@
 import { z } from "zod";
 
-//dados (usuário + professor)
-export type Professor = {
-    id: string
-    firebase_uid: string
-    email: string
-    password: string
-    papel: 'professor'
-    nome: string
-    cpf?: string
-    data_criacao: Date
-};
-
 export const createProfessorSchema = z.object({
     email: z.email({ message: "Email inválido" }),
     password: z.string().min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
