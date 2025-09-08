362c7d5f08ac865c0fea78f5f3be2ba4367957d4
João <joao_todescato@unochapeco.edu.br>
2025-09-08 15:18:28 -0300

    Add files via upload

---
+++ src/controllers/oficinas-controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { OficinasService } from "@/services/oficinas-services";
+
+class OficinaControllers {
+    
+    private oficinasService: OficinasService;
+
+    constructor(){
+        this.oficinasService = new OficinasService();
+    }
+
+    async index(req: Request, res: Response){
+        return res.status(200).json(await this.oficinasService.index());
+    };
+
+    async create(req: Request, res: Response){
+        const oficina = req.body;
+
+        await this.oficinasService.create(oficina);
+
+        return res.status(201).json({ message: "Oficina criada com sucesso!" });
+    }
+
+    async update(req: Request, res: Response){
+        const { id } = req.params;
+        const oficina = req.body;
+
+        await this.oficinasService.update(id, oficina);
+
+        return res.status(204).end();
+    }
+
+    async delete(req: Request, res: Response){
+        const { id } = req.params;
+
+        await this.oficinasService.delete(id);
+
+        return res.status(204).end();
+    }
+}
+
+export { OficinaControllers };
